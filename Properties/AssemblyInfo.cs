630d336c384a88dd7cef374da72c2401f0ba0379
Александр Краснов <akrasnov87@gmail.com>
2020-10-18 11:05:59 +0300

    update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // Общие сведения о сборке можно задать с помощью следующего 
 // атрибутов. Отредактируйте значения этих атрибутов, чтобы изменить
 // сведений о сборке.
-[assembly: AssemblyTitle("Семейный бюджет")]
-[assembly: AssemblyDescription("Приложение для обработки и анализа семейных расходов и доходов.")]
+[assembly: AssemblyTitle("Администрирование vNext Team Bot")]
+[assembly: AssemblyDescription("Приложение для наполнения Базы знаний")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Krasnov Inc.")]
